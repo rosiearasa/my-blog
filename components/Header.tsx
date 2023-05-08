daf4d4925b7734c029c539c66a2bb8f594cd918f
rosiearasa <rosearasa7522@gmail.com>
2023-05-08 14:38:57 -0400

    lint issues

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,9 +11,7 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
-           
-            </div>
+            <div className="mr-3"></div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
