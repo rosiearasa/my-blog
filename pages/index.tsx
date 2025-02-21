955747378437a2de6c49fbadf8159a386af1c802
rosiearasa <rosearasa7522@gmail.com>
2025-02-21 04:47:48 +0100

    error fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { InferGetStaticPropsType } from 'next'
 import { NewsletterForm } from 'pliny/ui/NewsletterForm'
 import { allBlogs } from 'contentlayer/generated'
 import type { Blog } from 'contentlayer/generated'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const MAX_DISPLAY = 5
 
